4f45993d6231fc311ee7874f22c9790de9a010e2
Kanken6174 <yorick.geoffre@orange.fr>
2022-01-04 11:10:47 +0100

    nb cailloux

--- Code/tournois/tournoiIAvsRandom.py
+++ Code/tournois/tournoiIAvsRandom.py
@@ -87,10 +87,10 @@ def tournoiIAvsRandom(nbCaillouxJ1, nbCaillouxJ2, choix, prediction):
 
 
 if (True):
-    [choix, prediction] = loadModele("+2")      # chargement des différents modèles
+    [choix, prediction] = loadModele("std")      # chargement des différents modèles
 
     nbGames = 100       # nombre de partie par "manche de test"
-    nbCaillouxJ1 = 3    # nombre de cailloux du joueur 1
-    nbCaillouxJ2 = 3    # nombre de cailloux du joueur 2
+    nbCaillouxJ1 = 5    # nombre de cailloux du joueur 1
+    nbCaillouxJ2 = 5    # nombre de cailloux du joueur 2
 
     tournoiIAvsRandom(nbCaillouxJ1, nbCaillouxJ2, choix, prediction)
